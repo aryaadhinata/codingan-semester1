ba55cf199d1227cf51a665390f54611e1de7ef8d
Arya <148077534+aryaadhinata@users.noreply.github.com>
2025-11-16 20:31:49 +0700

    pass by refence

--- tugasDasPro/tugas06/main.c
+++ tugasDasPro/tugas06/main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
-void turn(int i, char nama[], int damage[i], char tipe[], int Ehp, char Etipe[]){
+void turn(int i, char nama[], int damage, char tipe[], int *Ehp, char Etipe[]){
 	if((strcmp(tipe, "Data") == 0) && (strcmp(Etipe, "Vaccine") == 0) ||
 		(strcmp(tipe, "Virus") == 0) && (strcmp(Etipe, "Data") == 0) ||
 		(strcmp(tipe, "Vaccine") == 0) && (strcmp(Etipe, "Virus") == 0)){
-		damage[i] *= 2;
+		damage *= 2;
 	}
 	
 	printf("|--- %s memberikan %d damage ke Myostismon\n", nama, damage);
-	Ehp -= damage[i];
+	*Ehp -= damage;
 }
 
-void Eturn(int i, char nama[], int hp, int Edamage,  char tipe[], char Etipe[]){
+void Eturn(int i, char nama[], int *hp, int Edamage,  char tipe[], char Etipe[]){
 	if((strcmp(Etipe, "Data") == 0) && (strcmp(tipe, "Vaccine") == 0) ||
 		(strcmp(Etipe, "Virus") == 0) && (strcmp(tipe, "Data") == 0) ||
 		(strcmp(Etipe, "Vaccine") == 0) && (strcmp(tipe, "Virus") == 0)){
@@ -20,30 +20,66 @@ void Eturn(int i, char nama[], int hp, int Edamage,  char tipe[], char Etipe[]){
 	}
 	
 	printf("/--- %s memberikan %d damage ke Myostismon\n", nama, Edamage);
-	hp -= Edamage;
+	*hp -= Edamage;
 }
 
 void status(char nama[], int hp){
-	printf("Sisa HP %s sekarang %d\n", nama, &hp);
+	if(hp <= 0){
+		printf("Sisa HP %s sekarang 0\n", nama);		
+	}else{
+		printf("Sisa HP %s sekarang %d\n", nama, hp);		
+	}
 }
 
-void pertarungan(int n, char nama[][129], int *hp[], int damage[], char rank[][129], char tipe[][129], int *Ehp, int Edamage, char Etipe[]){
-	for(int h = 0; h < n; h++){
+void Estatus(int Ehp, int *l){
+	if(Ehp <= 0){
+		printf("Sisa HP Myostismon sekarang 0\n");
+		*l -= 1;
+	}else{
+		printf("Sisa HP Myostismon sekarang %d\n", Ehp);
+	}
+}
+
+void pertarungan(int h, int n, int l, char nama[][129], int hp[], int damage[], char tipe[][129], int Ehp, int Edamage, char Etipe[]){
+	while((h < n) && (Ehp >= 0)){
 		printf("+--+--+--++--+--+--++--+--+--++--+--+--++--+--+--++--+--+--++--+--+--+\n");
 		printf("Saatnya menyerang Kege1apan (> ^ <)\n");
 		for(int i = 0; i < 3; i++){
-			turn(i, nama[i], damage[i], tipe[i], Ehp, Etipe);
+			turn(i, nama[i], damage[i], tipe[i], &Ehp, Etipe);
 		}
 
 		printf("\nHati - hati, Myostismon akan menyerang (,,>__<,,)\n");
 		for(int i = 0; i < 3; i++){
-			Eturn(i, nama[i], hp[i], Edamage, tipe[i], Etipe);
+			Eturn(i, nama[i], &hp[i], Edamage, tipe[i], Etipe);
 		}
 		
 		printf("\n");
 		for(int i = 0; i < 3; i++){
 			status(nama[i], hp[i]);
 		}
+		
+		printf("\n");
+		Estatus(Ehp, &l);
+		h++;
+	}
+}
+
+void endWorld(int h, int n, int *l, int Ehp, char nama[][129], char Fnama[][129], int hp[], int Fhp[], char rank[][129], char Frank[][129], int damage[], int Fdamage[]){
+	printf("+--+--+--++--+--+--++--+--+--++--+--+--++--+--+--++--+--+--++--+--+--+\n");
+	int decide = 3;
+	for(int i = 0; i < 3; i++){
+		if(hp[i] <= 0){
+			decide--; 
+		}
+	}
+	
+	printf("%d", l);
+	if(Ehp <= 0){
+		printf("Yeayy, kita memang anak anak terpilih\n");
+	}else if(decide == 0){
+		printf("Nooo, dunia akan hancur\n");
+	}else if(h == n){
+		printf("Siall, Myostismoon Kabur\n");
 	}
 }
 
@@ -79,6 +115,10 @@ int main(){
 	char sort[17];
 	scanf(" %s", sort);
 	
-	pertarungan(n, nama, &hp, damage, rank, tipe, &Ehp, Edamage, Etipe);
+	int h = 0;
+	int l = 1;
+	pertarungan(h, n, l, nama, hp, damage, tipe, Ehp, Edamage, Etipe);
+
+	endWorld(h, n, &l, Ehp, nama, Fnama, hp, Fhp, rank, Frank, damage, Fdamage);
 	return 0;
 }
